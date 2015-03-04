ec05a4b5db62f384e9e421f5120bf42f0a2e2632
Kurt Probe <alexprobe@gmail.com>
2015-03-04 09:33:30 -0600

    All VISs formatted

--- 03 Visualizations/DataWrangling3 - Visualization.R
+++ 03 Visualizations/DataWrangling3 - Visualization.R
@@ -1,5 +1,18 @@
 VIS3 <- DATA3 %>% ggplot(aes(x = UNEMPLOYMENT_RATE_AVG, y = INTEREST_RATE_AVG))+
-  geom_point(aes(color=STATE))
+  geom_point(aes(color=STATE))+ geom_text(aes(label=STATE, hjust=1.5), size=3); VIS3
+
+#Add and Format Title
+VIS3 <- VIS3 +ggtitle('Unemployment Rate vs. Average Interest Rate') +
+  theme(plot.title=element_text(size=18, face="bold", vjust=2)); VIS3
+#Format Axis labels
+VIS3 <- VIS3 + labs(x="Unemployment Rate (%)", y=paste(" Average Interest Rate (%)")) + theme(
+  axis.title.x=element_text(face="bold", vjust=0.35),
+  axis.title.y=element_text(face="bold", vjust=0.35)); VIS3
+#Basic Formatting
+VIS3 <- VIS3 + theme(panel.background = element_rect(fill = 'grey92',color = 'grey1')) + theme(legend.position='none'); VIS3
+
+
+
 
 CORR3 <- cor(DATA1[,2:3])
 
